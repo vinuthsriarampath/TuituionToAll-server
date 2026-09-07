6bba6d9667d9a48bca993267262f54cf830a3e3b
vinuth <vinuthsriarampath@outlook.com>
2026-09-07 10:40:27 +0530

    feat: add endpoint to retrieve detailed student course information by course and batch ID

--- src/main/java/edu/vinu/domain/student/controller/StudentController.java
+++ src/main/java/edu/vinu/domain/student/controller/StudentController.java
@@ -14,6 +14,7 @@
 package edu.vinu.domain.student.controller;
 
 import edu.vinu.common.response.ApiResponse;
+import edu.vinu.domain.course.service.StudentCourseService;
 import edu.vinu.domain.student.dto.request.StudentDetailsUpdateRequest;
 import edu.vinu.domain.student.dto.response.Student;
 import edu.vinu.domain.student.service.StudentLearningService;
@@ -33,6 +34,7 @@ import static org.springframework.http.HttpStatus.OK;
 public class StudentController {
     private final StudentService studentService;
     private final StudentLearningService studentLearningService;
+    private final StudentCourseService studentCourseService;
 
     @PreAuthorize("hasAuthority('student')")
     @PatchMapping("/me")
@@ -57,5 +59,11 @@ public class StudentController {
     @GetMapping("/me/learning/courses/{courseId}/enrollment-history")
     public ResponseEntity<ApiResponse> getEnrollmentHistory(@PathVariable Long courseId) {
         return ResponseEntity.ok(studentLearningService.getEnrollmentHistory(courseId));
+    }
+
+    @PreAuthorize("hasAuthority('student')")
+    @GetMapping("/me/learning/courses/{courseId}/batches/{batchId}")
+    public ResponseEntity<ApiResponse> getStudentCourseDetails(@PathVariable Long courseId, @PathVariable Long batchId){
+        return ResponseEntity.ok(studentCourseService.getStudentDetailedCourse(courseId, batchId));
     }
 }
